f86b413bd16d455c6cc5eba81c2ff49795ac86f6
Heitor <heitorschleder33@gmail.com>
2025-03-14 09:43:39 -0300

    added validation to form fields
    With this update we prevent the user from encountering an error when requesting information from the API, we also save API calls

--- src/components/ShippingForm.vue
+++ src/components/ShippingForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed, watch } from 'vue';
 import { useShippingStore } from '../stores/shipping';
 import type { ShippingQuoteForm } from '../types/shipping';
 import { formatPostalCode } from '../utils/formatPostalCode';
@@ -23,10 +23,30 @@ const form = ref<ShippingQuoteForm>({
   recipientCountry: 'BR'
 });
 
+const errors = ref<{ [key: string]: string }>({});
+
+const validateForm = () => {
+  errors.value = {};
+  if (!form.value.sellerCEP) errors.value.sellerCEP = 'Informe o CEP de origem';
+  if (!form.value.recipientCEP) errors.value.recipientCEP = 'Informe o CEP de destino';
+  if (form.value.invoiceValue <= 0) errors.value.invoiceValue = 'O valor deve ser maior que 0';
+  if (form.value.items[0].weight <= 0) errors.value.weight = 'Informe um peso válido';
+  if (form.value.items[0].width <= 0) errors.value.width = 'Informe a largura';
+  if (form.value.items[0].height <= 0) errors.value.height = 'Informe a altura';
+  if (form.value.items[0].length <= 0) errors.value.length = 'Informe o comprimento';
+};
+
+const isFormValid = computed(() => Object.keys(errors.value).length === 0);
+
+watch(form, validateForm, { deep: true });
+
 const handleSubmit = async () => {
+  validateForm();
+  if (!isFormValid.value) return;
   await store.calculateShipping(form.value);
 };
 </script>
+
 <template>
   <form @submit.prevent="handleSubmit" class="max-w-lg lg:max-w-2xl mx-auto p-6 lg:p-10 bg-white rounded-lg shadow-md">
     <h2 class="text-2xl lg:text-3xl font-bold mb-6">Simule seu Frete</h2>
@@ -36,57 +56,56 @@ const handleSubmit = async () => {
         <div>
           <label class="block text-sm lg:text-base font-medium text-gray-700">CEP de Origem</label>
           <input type="text" v-model="form.sellerCEP" @input="e => form.sellerCEP = formatPostalCode((e.target as HTMLInputElement).value)" maxlength="9" placeholder="00000-000"
-  class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 lg:text-lg lg:p-3" />
+            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 lg:text-lg lg:p-3" />
+          <p v-if="errors.sellerCEP" class="text-red-600 text-sm">{{ errors.sellerCEP }}</p>
         </div>
 
-        <div class="relative">
+        <div>
           <label class="block text-sm lg:text-base font-medium text-gray-700">CEP de Destino</label>
-          <div class="flex items-center mt-1">
-            <input type="text" v-model="form.recipientCEP" @input="e => form.recipientCEP = formatPostalCode((e.target as HTMLInputElement).value)" maxlength="9" placeholder="00000-000"
-  class="block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 pr-20 lg:text-lg lg:p-3" />
-            <a href="https://buscacepinter.correios.com.br/app/endereco/index.php" target="_blank"
-              class="text-blue-500 text-xs lg:text-sm hover:underline absolute right-3">
-              Não sei o CEP
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <!-- Info prod -->
-      <div class="grid grid-cols-2 lg:grid-cols-2 gap-4">
-        <div>
-          <label class="block text-sm lg:text-base font-medium text-gray-700">Peso (kg)</label>
-          <input type="number" v-model="form.items[0].weight" min="0" step="0.1" placeholder="0"
+          <input type="text" v-model="form.recipientCEP" @input="e => form.recipientCEP = formatPostalCode((e.target as HTMLInputElement).value)" maxlength="9" placeholder="00000-000"
             class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 lg:text-lg lg:p-3" />
-        </div>
-        <div>
-          <label class="block text-sm lg:text-base font-medium text-gray-700">Valor (R$)</label>
-          <input type="number" v-model="form.invoiceValue" min="0" step="0.01" placeholder="0.00"
-            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 lg:text-lg lg:p-3" />
+          <p v-if="errors.recipientCEP" class="text-red-600 text-sm">{{ errors.recipientCEP }}</p>
         </div>
       </div>
 
       <div class="grid grid-cols-2 gap-4">
         <div>
+          <label class="block text-sm lg:text-base font-medium text-gray-700">Peso (kg)</label>
+          <input type="number" v-model="form.items[0].weight" min="0" step="0.1" placeholder="0"
+            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 lg:text-lg lg:p-3" />
+          <p v-if="errors.weight" class="text-red-600 text-sm">{{ errors.weight }}</p>
+        </div>
+        <div>
+          <label class="block text-sm lg:text-base font-medium text-gray-700">Valor (R$)</label>
+          <input type="number" v-model="form.invoiceValue" min="0" step="0.01" placeholder="0.00"
+            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 lg:text-lg lg:p-3" />
+          <p v-if="errors.invoiceValue" class="text-red-600 text-sm">{{ errors.invoiceValue }}</p>
+        </div>
+      </div>
+
+      <div class="grid grid-cols-3 gap-4">
+        <div>
           <label class="block text-sm lg:text-base font-medium text-gray-700">Largura (cm)</label>
           <input type="number" v-model="form.items[0].width" min="0" placeholder="0"
             class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 lg:text-lg lg:p-3" />
+          <p v-if="errors.width" class="text-red-600 text-sm">{{ errors.width }}</p>
         </div>
         <div>
           <label class="block text-sm lg:text-base font-medium text-gray-700">Altura (cm)</label>
           <input type="number" v-model="form.items[0].height" min="0" placeholder="0"
             class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 lg:text-lg lg:p-3" />
+          <p v-if="errors.height" class="text-red-600 text-sm">{{ errors.height }}</p>
         </div>
-      </div>
-      
-      <div>
-        <label class="block text-sm lg:text-base font-medium text-gray-700">Comprimento (cm)</label>
-        <input type="number" v-model="form.items[0].length" min="0" placeholder="0"
-          class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 lg:text-lg lg:p-3" />
+        <div>
+          <label class="block text-sm lg:text-base font-medium text-gray-700">Comprimento (cm)</label>
+          <input type="number" v-model="form.items[0].length" min="0" placeholder="0"
+            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 lg:text-lg lg:p-3" />
+          <p v-if="errors.length" class="text-red-600 text-sm">{{ errors.length }}</p>
+        </div>
       </div>
     </div>
     
-    <button type="submit" :disabled="store.isLoading"
+    <button type="submit" :disabled="Object.keys(errors).length > 0"
       class="mt-6 w-full bg-blue-900 text-white py-2 px-4 lg:py-3 lg:px-6 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 flex items-center justify-center gap-2 lg:text-lg">
       <Loading v-if="store.isLoading" />
       {{ store.isLoading ? 'Calculando...' : 'Calcular' }}
